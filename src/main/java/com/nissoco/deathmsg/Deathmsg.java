39dfd16deeb5324c6047122921ae11b549e0c052
Nissoco23 <nissocomc@outlook.com>
2025-12-06 21:53:22 -0600

    Initialize Project

---
+++ src/main/java/com/nissoco/deathmsg/Deathmsg.java
@@ -0,0 +1,17 @@
+package com.nissoco.deathmsg;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Deathmsg extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
